a259974de4c4f3c5140f01618250afc4a4bd2d21
mrgauz <github@gauz.net>
2020-11-24 20:02:56 +0100

    Add skeleton for Tweet model

---
+++ Models/Tweet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TwitterBirdBot.Models
+{
+    public class Tweet
+    {
+        public string Id { get; set; }
+        public string UserHandle { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Text { get; set; }
+
+        public string ToMessage()
+        {
+            // TODO - Gauz
+            return "";
+        }
+    }
+}
